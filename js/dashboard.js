37389d73278796f6c924cbc767af94b3022518ae
Abner Suhett <suhettabner@gmail.com>
2023-02-12 10:39:17 -0300

    adding comments

--- js/dashboard.js
+++ js/dashboard.js
@@ -14,9 +14,13 @@ function renderNotes() {
   notes.forEach((note, index) => {
     //note = cada objeto, index = indice de cada objeto
     divNotes.innerHTML += `<div id=${index} class="inputGroup">
-            <input onchange='checkNote' type="checkbox" class="check" checked=${note.checked}/>
+            <input onchange='checkNote' type="checkbox" class="check" checked=${
+              note.checked ? "checked" : ""
+            }/>
             <input disabled class="itemTitle" value="${note.title}"></input>
-            <input disabled class="itemDescription" value="${note.description}"></input>
+            <input disabled class="itemDescription" value="${
+              note.description
+            }"></input>
             <button class='btn-edit' onclick='editNote(this)'><i class="ph-pencil"></i></button>
             <button class='btn-save' onclick='updateNote(this)' style='display:none'><i class="ph-check"></i></button>
             <button onclick='deleteNote(this)'><i class="ph-trash"></i></button>
@@ -25,20 +29,19 @@ function renderNotes() {
   });
 }
 
+//Adicionar nova nota
 formNewItem.addEventListener("submit", (e) => {
   e.preventDefault();
 
   note = {
     title: ItemTitle.value,
     description: ItemDescription.value,
-    checked: true,
+    checked: "",
   };
 
   if (
-    !ItemTitle.value ||
-    ItemTitle.value === " " ||
-    !ItemDescription.value ||
-    ItemDescription.value === " "
+    ItemTitle.value.trim().length === 0 ||
+    ItemDescription.value.trim().length === 0
   ) {
     errorMessage.innerText = `Por favor digite a tarefa antes de adicionar.`;
   } else {
@@ -54,14 +57,20 @@ formNewItem.addEventListener("submit", (e) => {
   console.log(notes);
 });
 
-function deleteNote(deletedElement) {
-  notes.splice(+deletedElement.parentElement.id, 1);
+//deletar nota
+function deleteNote(deleteBtn) {
+  //transformando o #id da div pai do botão em número e excluindo 1 item
+  notes.splice(+deleteBtn.parentElement.id, 1);
+
+  //Renderiza novamente as notas
   renderNotes();
-  //   console.log(+deletedElement.parentElement.id);
+
   console.log(notes);
 }
 
+//Editar nota
 function editNote(editBtn) {
+  //pegando os inputs a partir do botão editar
   let newTitle = editBtn.parentElement.querySelector(".itemTitle");
   let newDescription = editBtn.parentElement.querySelector(".itemDescription");
 
@@ -72,7 +81,9 @@ function editNote(editBtn) {
   newDescription.disabled = false;
 }
 
+//Atualizar Nota
 function updateNote(updateBtn) {
+  //pegando os inputs dentro da div pai e o ID da div pelo botão editar
   let newTitle = updateBtn.parentElement.querySelector(".itemTitle");
   let newDescription =
     updateBtn.parentElement.querySelector(".itemDescription");
@@ -84,6 +95,7 @@ function updateNote(updateBtn) {
   newTitle.disabled = true;
   newDescription.disabled = true;
 
+  //atualizando novos valores no objeto
   note = notes[getId];
   note.title = newTitle.value;
   note.description = newDescription.value;
@@ -91,4 +103,8 @@ function updateNote(updateBtn) {
   console.log(notes);
 }
 
-function checkNote(checkBtn) {}
+//Check Nota
+function checkNote(checkBtn) {
+  let isChecked = checkBtn.parentElement.querySelector(".check");
+  note.checked = isChecked.value;
+}
