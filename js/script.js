aaa990d8be19c60f46b557a3d4c7c6f2a9d1ba28
Abner Suhett <suhettabner@gmail.com>
2023-03-02 12:14:15 -0300

    fazendo login e verificações

--- js/script.js
+++ js/script.js
@@ -1,117 +1,185 @@
-//Botão div Bem vindo
-let changeFormBtn = document.querySelector("#changeForm");
-let p = document.querySelector("#changeForm p");
+const changeFormBtn = document.querySelector("#changeForm");
+const p = document.querySelector("#changeForm p");
 const divLogin = document.querySelector("#divLogin");
 const divSignup = document.querySelector("#signup");
 
-let username = document.querySelector("#newUsername");
-let usernameLabel = document.querySelector("#newUserlabel");
+const signUpForm = document.querySelector("#signupForm");
+const loginForm = document.querySelector("#loginForm");
+
+let newUsername = signUpForm.newUsername;
+let newUserLabel = signUpForm.querySelector("#newUserlabel");
 let validUser = false;
 
-let email = document.querySelector("#email");
-let emailLabel = document.querySelector("#emailLabel");
+let newEmail = signUpForm.email;
+let newEmailLabel = signUpForm.querySelector("#emailLabel");
 let validEmail = false;
 
-let password = document.querySelector("#newPassword");
-let passwordLabel = document.querySelector("#passwordLabel");
+let newPassword = signUpForm.password
+let newPasswordLabel = signUpForm.querySelector("#passwordLabel");
 let validPassword = false;
 
-let password2 = document.querySelector("#password2");
-let password2Label = document.querySelector("#password2Label");
+let password2 = signUpForm.password2;
+let assword2Label = signUpForm.querySelector("#password2Label");
 let validPassword2 = false;
 
-changeFormBtn.addEventListener("click", function () {
-  //Ao clicar no botão alterna a adição e remoçao da class hidden nas divs
-  divLogin.classList.toggle("hidden");
-  divSignup.classList.toggle("hidden");
+////////////////////////////////////////////////////////////
 
-  if (divLogin.classList.contains("hidden")) {
-    p.innerHTML = "Acesse Sua conta!";
-  } else {
-    p.innerHTML = "Cadastre-se";
-  }
+//Muda o formulário alternando a class através do botão
+function changeForm(){
+        divLogin.classList.toggle("hidden");
+        divSignup.classList.toggle("hidden");
+      
+        if (divLogin.classList.contains("hidden")) {
+          p.innerHTML = "Acesse Sua conta!";
+        } else {
+          p.innerHTML = "Cadastre-se";
+        }
+    
+}
+////////////////////////////////////////////////////////////
+
+//verificação campo novo usuário
+newUsername.addEventListener("keyup", () => {
+    if (newUsername.value.length <= 2 || newUsername.value.trim() === '') {
+      newUsername.setAttribute("style", "border-color: red");
+      newUserLabel.innerText = "Insira no minimo 3 caracteres";
+      validUser = false;
+
+    } else {
+      newUsername.setAttribute("style", "border-color: none");
+      newUserLabel.innerText = "Usuário";
+      validUser = true;
+    }
 });
 
-//Verificações de formulário Cadastro
-
-//Verificando e mudando o estilo do usuario caso o numero de caracteres seja inválido
-username.addEventListener("keyup", () => {
-  if (username.value.length <= 2) {
-    username.setAttribute("style", "border-color: red");
-    usernameLabel.innerText = "Insira no minimo 3 caracteres";
-    validUser = false;
-  } else {
-    username.setAttribute("style", "border-color: green");
-    usernameLabel.innerText = "Usuário";
-    validUser = true;
-  }
+//verificação campo novo email
+newEmail.addEventListener("keyup", () => {
+    if (newEmail.value.length <= 6 || !newEmail.value.includes('@')) {
+      newEmail.setAttribute("style", "border-color: red");
+      newEmailLabel.innerText = "Insira um email válido";
+      validEmail = false;
+    } else {
+      newEmail.setAttribute("style", "border-color: none");
+      newEmailLabel.innerText = "E-mail";
+      validEmail = true;
+    }
 });
 
-//Verificando e mudando o estilo caso o numero de caracteres seja inválido
-email.addEventListener("keyup", () => {
-  if (email.value.length <= 6) {
-    email.setAttribute("style", "border-color: red");
-    emailLabel.innerText = "Por favor insira um email válido";
-    validEmail = false;
-  } else {
-    email.setAttribute("style", "border-color: green");
-    emailLabel.innerText = "E-mail";
-    validEmail = true;
-  }
+//verificação campo nova senha
+newPassword.addEventListener("keyup", () => {
+    if (newPassword.value.length <= 4 || newPassword.value.trim() === ' ') {
+      newPassword.setAttribute("style", "border-color: red");
+      newPasswordLabel.innerText = "Minimo 5 caracteres";
+      validPassword = false;
+    } else {
+      newPassword.setAttribute("style", "border-color: none");
+      newPasswordLabel.innerText = "Senha";
+      validPassword = true;
+    }
 });
 
-//Verificando e mudando o estilo caso o numero de caracteres seja inválido
-password.addEventListener("keyup", () => {
-  if (password.value.length <= 4) {
-    password.setAttribute("style", "border-color: red");
-    passwordLabel.innerText = "Minimo 5 caracteres";
-    validPassword = false;
-  } else {
-    password.setAttribute("style", "border-color: green");
-    passwordLabel.innerText = "Senha";
-    validPassword = true;
-  }
-});
-
-//Verificando e mudando o estilo caso as senhas não conferirem
+//verificação campo confirmação de senha
 password2.addEventListener("keyup", () => {
-  if (password2.value != password.value || password2.value == "") {
-    password2.setAttribute("style", "border-color: red");
-    password2Label.innerText = "As senhas não conferem";
-    validPassword2 = false;
-  } else {
-    password2.setAttribute("style", "border-color: green");
-    password2Label.innerText = "Senha Confirmada!";
-    validPassword2 = true;
-  }
-});
-
-//Formulário de Cadastro
-function signUp(e) {
-  e.preventDefault()
-  //Verifica se todos os inputs são válidos
-  if (validUser && validEmail && validPassword && validPassword2) {
-    alert("TUDO OK!");
-  } else {
-    alert("Insira os dados corretamente para prosseguir!");
-    return;
-  }
-
-  // Recebe os usuários do localStorage, caso não haja, inicia uma lista vazia
-
-  let users = JSON.parse(localStorage.getItem("users") || "[]");
-
-  //Adiciona outros usuários à lista de usuarios cadastrados
-  users.push({
-    username: username.value,
-    email: email.value,
-    password: password.value,
-    password2: password2.value,
+    if (password2.value != newPassword.value || password2.value == "") {
+      password2.setAttribute("style", "border-color: red");
+      password2Label.innerText = "As senhas não conferem";
+      validPassword2 = false;
+    } else {
+      password2.setAttribute("style", "border-color: none");
+      password2Label.innerText = "Senha Confirmada!";
+      validPassword2 = true;
+    }
   });
 
-  //Envia para o localStorage os usuários cadastrados
-  localStorage.setItem("users", JSON.stringify(users));
-  alert("Usuario Cadastrado!");
+////////////////////////////////////////////////////////////
+
+//Busca o item Users do localStorage, caso não tenha nenhum, retorna uma lista vazia
+let users = JSON.parse(localStorage.getItem("users") || "[]");
+
+// Função para cadastrar Usuário
+signUpForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    //verifica se todos os campos são válidos
+    if (validUser && validEmail && validPassword && validPassword2) {
+     let exists = verifyUserAlreadyExists() 
+
+      //se o usuário exitir, retorna como undefined e é necessario mudar o username e o email
+     if(exists === true){
+      return undefined;
+     } 
+    }
+
+    //se não existir, o usuário é adicionado ao localStorage
+    users.push({
+      username: newUsername.value,
+      email: newEmail.value,
+      password: newPassword.value
+    })
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+      signUpForm.reset()
+      
+})
+
+//Verifica se o usuário já existe
+function verifyUserAlreadyExists(exists){
+  let userExists = users.find((user)=> newUsername.value === user.username || newEmail.value === user.email)
+
+  //Se não existir o usuário ele cadastra
+  if(!userExists){
+    exists = false
+    document.querySelector("#signup h2").innerText = "Cadastre-se"
+    newUsername.setAttribute("style", "border-color: none")
+    newEmail.setAttribute("style", "border-color: none") 
+    alert("Usuário cadastrado com sucesso")
+    changeForm();
+    return exists;
+  }
+
+  //se existir, aparece um erro no formulário
+  if(userExists){
+    exists = true
+    document.querySelector("#signup h2").innerText = "Usuário ou email já cadastrado!"
+    newUsername.setAttribute("style", "border-color: red")
+    newEmail.setAttribute("style", "border-color: red")
+    return exists;
+  }
+   
+  
 }
 
-//Formulário de Login
+////////////////////////////////////////////////////////////////////
+//Função para fazer Login
+
+loginForm.addEventListener('submit', (e)=>{
+  e.preventDefault()
+
+  verifyUser(users) 
+})
+
+//Verifica os usuários
+function verifyUser(users) {
+ 
+  let loggedUser;
+
+  //percorre o localStorage
+   let user = users.find((user)=> loginForm.username.value === user.username && loginForm.password.value === user.password)
+  
+    //Se o usuário for válido, adiciona ao localStorage o Objeto LoggedUser para monitorar a sessão ativa
+    if (user) {
+     loggedUser = user
+     loggedUser.notes = []
+     console.log(loggedUser)
+     localStorage.setItem("loggedUser", JSON.stringify(loggedUser))
+     window.location.href = "dashboard.html"
+
+    //Se não for válido aparece um erro no formulário  
+    } else { 
+    document.querySelector(".form h2").innerText = "Usuário ou senha Incorretos"
+    loginForm.username.setAttribute("style", "border: 2px solid red")
+    loginForm.password.setAttribute("style", "border: 2px solid red")
+    }
+  
+}
